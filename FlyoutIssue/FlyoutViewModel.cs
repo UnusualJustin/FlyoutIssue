5d130ea06e11b9bdbfd118c9874a820ce5472b08
Justin Cook <justincook2003.work@gmail.com>
2023-11-16 09:27:16 -0600

    Add project files.

---
+++ FlyoutIssue/FlyoutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace FlyoutIssue
+{
+    internal class FlyoutViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool _isFlyoutOpen;
+
+        public bool IsFlyoutOpen
+        {
+            get => _isFlyoutOpen;
+            set
+            {
+                _isFlyoutOpen = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsFlyoutOpen)));
+            }
+        }
+    }
+}
---
+++ FlyoutIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlyoutIssue
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
